780e01d6fc10ed3a32d0310c78b81738ed4cc76e
WandersonK <wan01dez0013@gmail.com>
2021-09-20 14:41:46 -0300

    exemplos da aula 1

---
+++ Formação/JavaScript para back-end/Fundamentos do JavaScript - Tipos, variáveis e funções/codigos-aula/type-string.js
@@ -0,0 +1,28 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'A Ju disse "Oi!"';
+const citacao2 = "A Ju disse 'Oi!'";
+// console.log(citacao);
+
+// Concatenação (+)
+const citacao3 = "Meu nome é ";
+const meuNome = "Wanderson";
+
+console.log(citacao3 + meuNome);
+
+
+// Template String ou Template Literal
+
+// Transformação/conversão do código Unicode em caractere
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
