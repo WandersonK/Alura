8b871dcbb8d9f780495a780d5431d88ad3859afc
Wanderson Silva Rodrigues <wan01dez0013@gmail.com>
2021-09-20 19:54:44 -0300

    Exemplos

---
+++ Formação/JavaScript para back-end/Fundamentos do JavaScript - Tipos, variáveis e funções/codigos-aula/conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = "456";
+
+// console.log(numero == numeroString);
+
+// console.log(numero + numeroString); //Ocorre a concatenação, por possuir uma String na variável
+
+// conversão explícita
+
+// Number()
+// String()
+
+console.log(numero + Number(numeroString));
