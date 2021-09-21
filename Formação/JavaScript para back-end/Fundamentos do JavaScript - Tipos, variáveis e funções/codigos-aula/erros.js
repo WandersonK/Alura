5181ad1562b3cdb4d681e129603f5cd9b846bf02
WandersonK <wan01dez0013@gmail.com>
2021-09-21 17:58:32 -0300

    Exemplos Aula

---
+++ Formação/JavaScript para back-end/Fundamentos do JavaScript - Tipos, variáveis e funções/codigos-aula/erros.js
@@ -0,0 +1,4 @@
+const numero = 0;
+const alura = "oi;
+// console.log(numero);
+console.log(alura);
---
+++ Formação/JavaScript para back-end/Fundamentos do JavaScript - Tipos, variáveis e funções/codigos-aula/tipagem.js
@@ -0,0 +1,7 @@
+// tipagem dinâmica
+
+let minhaVar = 567;
+minhaVar = "texto";
+
+// tipagem interpretada
+// Necessita de um compilador
