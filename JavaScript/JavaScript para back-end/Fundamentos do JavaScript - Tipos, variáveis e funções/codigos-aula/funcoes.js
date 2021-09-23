5a341bfdbdbe7566c7b768b246a604365cacd407
WandersonK <wan01dez0013@gmail.com>
2021-09-23 19:28:21 -0300

    atualizaçao

---
+++ JavaScript/JavaScript para back-end/Fundamentos do JavaScript - Tipos, variáveis e funções/codigos-aula/funcoes.js
@@ -0,0 +1,17 @@
+// let x = "";
+// console.log(x);
+// x = "Oi";
+
+// 1) declara a função
+
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+// 2) executa a função (1 ou + vezes)
+
+imprimeTexto("oi")
+imprimeTexto("outro texto")
+
+// três formas de escrever funções
+
--- Python/Python 3 parte 1_Introdução à nova versão da linguagem/jogo_adivinhacao/adivinhacao.py
+++ Python/Python 3 parte 1_Introdução à nova versão da linguagem/jogo_adivinhacao/adivinhacao.py
@@ -1,60 +1,66 @@
 import random
 
-print("")
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
+def jogar():
 
-numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
-# rodada = 1 # Para utilizar com While
-pontos = 1000
-
-print("Qual nível de Dificuldade?")
-print("(1) Fácil (2) Médio (3) Difícil")
-
-nivel = int(input("Defina o nível: "))
-
-if nivel == 1:
-    total_de_tentativas = 20
-elif nivel == 2:
-    total_de_tentativas = 10
-else:
-    total_de_tentativas = 5
-
-
-for rodada in range(1, total_de_tentativas + 1):
-# while rodada <= total_de_tentativas:
     print("")
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    
-    chute = int(input("Digite o seu número: "))
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-    if(chute < 1 or chute > 100):
-        print("Você deve digitar um número entre 1 e 100!")
-        continue
+    numero_secreto = random.randrange(1,101)
+    total_de_tentativas = 3
+    # rodada = 1 # Para utilizar com While
+    pontos = 1000
 
-    acertou = chute == numero_secreto
-    chute_maior = chute > numero_secreto
-    chute_menor = chute < numero_secreto
+    print("Qual nível de Dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
+    nivel = int(input("Defina o nível: "))
 
-    if acertou:
-        print("Parabéns, você acertou e fez {} pontos.".format(pontos))
-        break
+    if (nivel == 1):
+        total_de_tentativas = 20
+    elif (nivel == 2):
+        total_de_tentativas = 10
     else:
-        pontos_perdidos = abs(numero_secreto - chute)
-        pontos -= pontos_perdidos
-        if chute_maior:
-            print("Você errou! Seu chute foi maior que o número secreto.")
-        elif chute_menor:
-            print("Você errou! Seu chute foi menor que o número secreto.")
-            
-    # rodada += 1 # Para utilizar com While
+        total_de_tentativas = 5
 
-if acertou:
-    print("")
-else:
-    print("Fim do Jogo, o número secreto era {}".format(numero_secreto))
 
-# print("Você finalizou com {}".format(pontos))
+    for rodada in range(1, total_de_tentativas + 1):
+    # while rodada <= total_de_tentativas:
+        print("")
+        print("Tentativa {} de {}.".format(rodada, total_de_tentativas))
+        
+        chute = int(input("Digite o seu número: "))
+
+        if(chute < 1 or chute > 100):
+            print("Você deve digitar um número entre 1 e 100!")
+            continue
+
+        acertou = chute == numero_secreto
+        chute_maior = chute > numero_secreto
+        chute_menor = chute < numero_secreto
+
+
+        if(acertou):
+            print("Parabéns, você acertou e fez {} pontos.".format(pontos))
+            break
+        else:
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos -= pontos_perdidos
+            if (chute_maior):
+                print("Você errou! Seu chute foi maior que o número secreto.")
+            elif (chute_menor):
+                print("Você errou! Seu chute foi menor que o número secreto.")
+                
+        # rodada += 1 # Para utilizar com While
+
+    if (acertou):
+        print("")
+    else:
+        print("Fim do Jogo, o número secreto era {}".format(numero_secreto))
+
+    # print("Você finalizou com {}".format(pontos))
+
+
+if (__name__ == "__main__"):
+    jogar()
