2f40c92616217304aebe9f61f5439f56999d1b90
WandersonK <wan01dez0013@gmail.com>
2021-09-24 16:54:09 -0300

    Finalizaçao do Curso

---
+++ JavaScript/JavaScript para back-end/Fundamentos do JavaScript - Tipos, variáveis e funções/codigos-aula/expressoes-funcao.js
@@ -0,0 +1,16 @@
+function minhaFuncao(param) {
+    // Bloco de código
+}
+
+// minhaFuncao(param);
+
+// expressão de função
+
+// diferença principal da função "soma" e "minhaFuncao": HOISTING
+// funções e var são "Listadas" no topo do arquivo.
+
+// console.log(soma(1, 1)); // Aqui a função soma não será executada
+
+const soma = function(num1, num2) { return num1 + num2 }
+
+// console.log(soma(1, 2)); // Aqui a função soma é executada
